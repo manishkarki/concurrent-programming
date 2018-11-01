ffa3a80b10c726d8b549768a7c8082725f832524
Manish Karki <manishkarki@USMBP.local>
2018-10-31 20:19:52 -0400

    assignment week 3 done

---
+++ miniproject_3/src/main/java/edu/coursera/concurrent/SieveActor1.java
@@ -0,0 +1,44 @@
+package edu.coursera.concurrent;
+
+import edu.rice.pcdp.Actor;
+
+import static edu.rice.pcdp.PCDP.finish;
+
+/**
+ * An actor-based implementation of the Sieve of Eratosthenes.
+ * <p>
+ * TODO Fill in the empty SieveActorActor actor class below and use it from
+ * countPrimes to determin the number of primes <= limit.
+ */
+public final class SieveActor1 extends Sieve {
+    /**
+     * {@inheritDoc}
+     * <p>
+     * TODO Use the SieveActorActor class to calculate the number of primes <=
+     * limit in parallel. You might consider how you can model the Sieve of
+     * Eratosthenes as a pipeline of actors, each corresponding to a single
+     * prime number.
+     */
+    @Override
+    public int countPrimes(final int limit) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * An actor class that helps implement the Sieve of Eratosthenes in
+     * parallel.
+     */
+    public static final class SieveActorActor extends Actor {
+        /**
+         * Process a single message sent to this actor.
+         *
+         * TODO complete this method.
+         *
+         * @param msg Received message
+         */
+        @Override
+        public void process(final Object msg) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
